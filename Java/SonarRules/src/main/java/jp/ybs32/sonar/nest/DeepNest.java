044630bedcc0b52701911df7b6e7bfdec4028507
yabushi <hdecogtrexi557960@gmail.com>
2019-08-10 22:00:42 +0900

    [Fix] Collapsed indent

--- Java/SonarRules/src/main/java/jp/ybs32/sonar/nest/DeepNest.java
+++ Java/SonarRules/src/main/java/jp/ybs32/sonar/nest/DeepNest.java
@@ -4,13 +4,13 @@ public class DeepNest {
 
 	public static void main(String[] args) {
 
-		if (args[0].equals("a")) {					// Compliant - depth = 1
+		if (args[0].equals("a")) { // Compliant - depth = 1
 			/* ... */
-			for (int i = 0; i < 10; i++) {			// Compliant - depth = 2
+			for (int i = 0; i < 10; i++) { // Compliant - depth = 2
 				/* ... */
-				if (args[1].equals("b")) {			// Compliant - depth = 3, not exceeding the limit
+				if (args[1].equals("b")) { // Compliant - depth = 3, not exceeding the limit
 					/* ... */
-					for (int j = 0; j < 10; j++) {	// Noncompliant - depth = 4
+					for (int j = 0; j < 10; j++) { // Noncompliant - depth = 4
 						/* ... */
 					}
 				}
