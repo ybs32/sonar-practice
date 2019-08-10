cdfc732c4e85bd791463e3bb046ce8b0ad1b8384
yabushi <hdecogtrexi557960@gmail.com>
2019-08-10 20:03:53 +0900

    [Add] Analysis targets of Java

---
+++ Java/SonarRules/src/main/java/jp/ybs32/sonar/switches/NonDefault.java
@@ -0,0 +1,28 @@
+package jp.ybs32.sonar.switches;
+
+public class NonDefault {
+
+	public static void main(String[] args) {
+
+		int arg = Integer.parseInt(args[0]);
+
+		// Missing default clause
+		switch (arg) {
+			case 0:
+				/* ... */
+				break;
+			case 1:
+				/* ... */
+				arg += 1;
+				break;
+			case 2:
+				/* ... */
+				arg += 2;
+				break;
+			// The final default clause is required for secure programming
+		}
+
+		System.out.println(arg);
+	}
+
+}
---
+++ Java/SonarRules/src/main/java/jp/ybs32/sonar/switches/NotLastDefault.java
@@ -0,0 +1,29 @@
+package jp.ybs32.sonar.switches;
+
+public class NotLastDefault {
+
+	public static void main(String[] args) {
+
+		int arg = Integer.parseInt(args[0]);
+
+		switch (arg) {
+			case 0:
+				/* ... */
+				break;
+			default:
+				// The default clause should be the last one
+				break;
+			case 1:
+				/* ... */
+				arg += 1;
+				break;
+			case 2:
+				/* ... */
+				arg += 2;
+				break;
+		}
+
+		System.out.println(arg);
+	}
+
+}
